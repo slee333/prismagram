ddc0b5b81c4bc8ddc8fae3e7a6ac89bb59cf5591
Seung Wook Lee <seungwooklee94@gmail.com>
2019-09-29 02:12:12 +0900

    Minor changes in datamodel. 기본 아바타 변경

--- src/api/Post/upload/upload.js
+++ src/api/Post/upload/upload.js
@@ -6,11 +6,15 @@ export default {
       isAuthenticated(request);
       const { user } = request;
       const { caption, files, location } = args;
+
+      // 일단 파일이 없는 post를 만들고
       const post = await prisma.createPost({
         caption,
         location,
         user: { connect: { id: user.id } }
       });
+
+      // 각각의 파일들을 만들어 post와 연결시켜주는 방식입니다.
       files.forEach(
         async file =>
           await prisma.createFile({

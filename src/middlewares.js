f43a1bc23512698b7a23634ca35beb29dcea8730
Seung Wook Lee <seungwooklee94@gmail.com>
2019-10-05 11:02:07 +0900

    병원 프로필 만들기 - GeoCode 받아오는 middleware 생성

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,6 +1,27 @@
+import axios from "axios";
+
 export const isAuthenticated = request => {
   if (!request.user) {
     throw Error("You need to log in to perform this action");
   }
   return;
-}; 
+};
+
+export const getGeoCode = async address => {
+  // Naver API로부터 주소의 Geocode를 가져옵니다.
+  const headers = {
+    "X-NCP-APIGW-API-KEY-ID": process.env.NCP_GEO_ID,
+    "X-NCP-APIGW-API-KEY": process.env.NCP_GEO_SECRET
+  };
+  const params = {
+    query: address
+  };
+  const config = { headers, params };
+  const {
+    data: { addresses }
+  } = await axios
+    .get("https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode", config)
+    .catch(error => console.log(error));
+
+  return addresses;
+};
--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 import "./env";
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
+import cors from "cors"
 import schema from "./schema";
 import "./passport";
 import { authenticateJwt } from "./passport";
-import { isAuthenticated } from "./middlewares";
+import { isAuthenticated, getGeoCode } from "./middlewares";
 import { uploadMiddleware, uploadController } from "./upload";
 
 const PORT = process.env.PORT || 4000;
@@ -14,6 +15,7 @@ const server = new GraphQLServer({
   context: ({ request }) => ({ request, isAuthenticated })
 });
 
+server.express.use(cors())
 server.express.use(logger("dev"));
 server.express.use(authenticateJwt);
 server.express.post("/api/upload", uploadMiddleware, uploadController);
